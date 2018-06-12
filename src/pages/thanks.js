a6ff55f9d73d5a82993dcda112c377cf00c5569f
Patricia bellardine <patriciabellardine@Patricias-MacBook-Pro.local>
2018-06-12 15:23:27 -0700

    last week updates, contact page and content

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default () => (
   <div>
     <h1>Thank you!</h1>
-    <p>This is a custom thank you page for form submissions</p>
+    <p>
+      Thank you kindly for your intrest. Im happy to get started on your next
+      big idea. I will hit you back up ASAP!
+    </p>
   </div>
 )
